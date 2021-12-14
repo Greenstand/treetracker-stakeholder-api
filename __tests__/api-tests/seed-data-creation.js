03e5f8cbe95f29a0940b904d19b6e843819080ef
Gwynn DP <gwynndp@gmail.com>
2021-12-13 18:01:21 -0800

    chore: disable api tests temporarily

--- __tests__/api-tests/seed-data-creation.js
+++ __tests__/api-tests/seed-data-creation.js
@@ -4,19 +4,19 @@ const knex = require('../../database/connection');
 const stakeholderOne = Object.freeze({
   id: 5000000,
   type: 'type',
-  name: 'name',
+  org_name: 'name',
   first_name: 'first_name',
   last_name: 'last_name',
   email: 'email',
   phone: 'phone',
-  pwd_reset_required: true,
+  // pwd_reset_required: true,
   website: 'website',
-  wallet: 'wallet@@#',
-  password: 'password',
-  salt: 'salt',
-  active_contract_id: 10,
-  offering_pay_to_plant: true,
-  tree_validation_contract_id: 11,
+  // wallet: 'wallet@@#',
+  // password: 'password',
+  // salt: 'salt',
+  // active_contract_id: 10,
+  // offering_pay_to_plant: true,
+  // tree_validation_contract_id: 11,
   logo_url: 'url',
   map: 'ma,e',
   stakeholder_uuid: uuid(),
@@ -24,32 +24,39 @@ const stakeholderOne = Object.freeze({
 const stakeholderTwo = Object.freeze({
   id: 5000001,
   type: 'type',
-  name: 'name',
+  org_name: 'name',
   first_name: 'first_name',
   last_name: 'last_name',
   email: 'email',
   phone: 'phone',
-  pwd_reset_required: true,
+  // pwd_reset_required: true,
   website: 'website',
-  wallet: 'wallet@!#',
-  password: 'password',
-  salt: 'salt',
-  active_contract_id: 10,
-  offering_pay_to_plant: true,
-  tree_validation_contract_id: 11,
+  // wallet: 'wallet@!#',
+  // password: 'password',
+  // salt: 'salt',
+  // active_contract_id: 10,
+  // offering_pay_to_plant: true,
+  // tree_validation_contract_id: 11,
   logo_url: 'url',
   map: 'ma,e',
   stakeholder_uuid: uuid(),
 });
 
 before(async () => {
-  await knex('entity').insert([stakeholderOne, stakeholderTwo]);
+  await knex('stakeholder').insert([stakeholderOne, stakeholderTwo]);
 });
+
+// after(async () => {
+//   await knex.raw(`
+//     DELETE FROM stakeholder
+//     WHERE password = '${stakeholderTwo.password}';
+//   `);
+// });
 
 after(async () => {
   await knex.raw(`
-    DELETE FROM entity
-    WHERE password = '${stakeholderTwo.password}';
+    DELETE FROM stakeholder
+    WHERE id = '${5000000}' OR id = '${5000001}';
   `);
 });
 
