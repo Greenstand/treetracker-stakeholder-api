44ef9754122b092cef02bd41f6bcd098e48373c1
deanchen <antlet2004@gmail.com>
2022-02-10 10:22:57 +0800

    chore: unsaved file

--- database/migrations/20220210020649-createFCCTieredConfigurationTable.js
+++ database/migrations/20220210020649-createFCCTieredConfigurationTable.js
@@ -1,25 +1,47 @@
-'use strict';
-
-var dbm;
-var type;
-var seed;
+let dbm;
+let type;
+let seed;
+const fs = require('fs');
+const path = require('path');
 
 /**
   * We receive the dbmigrate dependency from dbmigrate initially.
   * This enables us to not have to rely on NODE_PATH.
   */
-exports.setup = function(options, seedLink) {
+exports.setup = function (options, seedLink) {
   dbm = options.dbmigrate;
   type = dbm.dataType;
   seed = seedLink;
 };
 
-exports.up = function(db) {
-  return null;
+exports.up = function (db) {
+  const filePath = path.join(__dirname, 'sqls', '20220210020649-createFCCTieredConfigurationTable-up.sql');
+  return new Promise(function (resolve, reject) {
+    fs.readFile(filePath, { encoding: 'utf-8' }, function (err, data) {
+      if (err) return reject(err);
+      console.log('received data: ', data);
+
+      resolve(data);
+    });
+  })
+    .then(function (data) {
+      return db.runSql(data);
+    });
 };
 
-exports.down = function(db) {
-  return null;
+exports.down = function (db) {
+  const filePath = path.join(__dirname, 'sqls', '20220210020649-createFCCTieredConfigurationTable-down.sql');
+  return new Promise(function (resolve, reject) {
+    fs.readFile(filePath, { encoding: 'utf-8' }, function (err, data) {
+      if (err) return reject(err);
+      console.log('received data: ', data);
+
+      resolve(data);
+    });
+  })
+    .then(function (data) {
+      return db.runSql(data);
+    });
 };
 
 exports._meta = {
