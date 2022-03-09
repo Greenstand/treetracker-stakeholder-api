0e226097336bd87734386ee7b4c831720575b2b8
Zaven Arra <matthew.fullstackgis@gmail.com>
2022-03-09 15:09:26 -0800

    fix: added children function

---
+++ database/migrations/sqls/20220309230529-CreateChildrenFunction-up.sql
@@ -0,0 +1,21 @@
+CREATE FUNCTION getStakeholderChildren
+(
+  uuid
+)
+RETURNS TABLE (stakeholder_id uuid, parent_id uuid, depth int, relations_type text, relations_role text)
+AS $$
+WITH RECURSIVE children AS (
+   SELECT stakeholder.id, stakeholder_relation.parent_id, 1 as depth, stakeholder_relation.type, stakeholder_relation.role
+   FROM stakeholder
+   LEFT JOIN stakeholder_relation ON stakeholder_relation.child_id = stakeholder.id 
+   WHERE stakeholder.id = $1
+  UNION
+   SELECT next_child.id, stakeholder_relation.parent_id, depth + 1, stakeholder_relation.type, stakeholder_relation.role
+   FROM stakeholder next_child
+   JOIN stakeholder_relation ON stakeholder_relation.child_id = next_child.id 
+   JOIN children c ON stakeholder_relation.parent_id = c.id
+)
+SELECT *
+FROM children
+$$
+LANGUAGE SQL;
