f8f622ac18d3df0889e6ae770d436ae129e2c7c5
Gwynn DP <gwynndp@gmail.com>
2022-03-09 07:48:59 -0800

    fix: prevent knex from doing migrations on startup

--- server/server.js
+++ server/server.js
@@ -3,20 +3,10 @@ const log = require('loglevel');
 // setup log level
 require('./setup');
 const app = require('./app');
-const knex = require('../database/connection');
 
 const PORT = process.env.NODE_PORT || 3006;
 
-knex.migrate
-  .latest()
-  .then((migrations) => {
-    log.info('migrations', migrations);
-    app.listen(PORT, () => {
-      log.info(`Listening on Port ${PORT}!`);
-      log.debug('debug log level!');
-    });
-  })
-  .catch((error) => {
-    log.error(error);
-    knex.destroy();
-  });
+app.listen(PORT, () => {
+  log.info(`listening on port:${PORT}`);
+  log.debug('debug log level!');
+});
