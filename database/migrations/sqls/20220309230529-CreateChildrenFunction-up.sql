94388c7387640edc2520275bbfc1c4b3437ffa19
Zaven Arra <matthew.fullstackgis@gmail.com>
2022-03-09 17:12:35 -0800

    feat: add children function

--- database/migrations/sqls/20220309230529-CreateChildrenFunction-up.sql
+++ database/migrations/sqls/20220309230529-CreateChildrenFunction-up.sql
@@ -5,15 +5,15 @@ CREATE FUNCTION getStakeholderChildren
 RETURNS TABLE (stakeholder_id uuid, parent_id uuid, depth int, relations_type text, relations_role text)
 AS $$
 WITH RECURSIVE children AS (
-   SELECT stakeholder.id, stakeholder_relation.parent_id, 1 as depth, stakeholder_relation.type, stakeholder_relation.role
-   FROM stakeholder
-   LEFT JOIN stakeholder_relation ON stakeholder_relation.child_id = stakeholder.id 
-   WHERE stakeholder.id = $1
+   SELECT stakeholder.stakeholder.id, stakeholder.stakeholder_relation.parent_id, 1 as depth, stakeholder.stakeholder_relation.type, stakeholder.stakeholder_relation.role
+   FROM stakeholder.stakeholder
+   LEFT JOIN stakeholder.stakeholder_relation ON stakeholder.stakeholder_relation.child_id = stakeholder.stakeholder.id 
+   WHERE stakeholder.stakeholder.id = $1
   UNION
-   SELECT next_child.id, stakeholder_relation.parent_id, depth + 1, stakeholder_relation.type, stakeholder_relation.role
-   FROM stakeholder next_child
-   JOIN stakeholder_relation ON stakeholder_relation.child_id = next_child.id 
-   JOIN children c ON stakeholder_relation.parent_id = c.id
+   SELECT next_child.id, stakeholder.stakeholder_relation.parent_id, depth + 1, stakeholder.stakeholder_relation.type, stakeholder.stakeholder_relation.role
+   FROM stakeholder.stakeholder next_child
+   JOIN stakeholder.stakeholder_relation ON stakeholder.stakeholder_relation.child_id = next_child.id 
+   JOIN children c ON stakeholder.stakeholder_relation.parent_id = c.id
 )
 SELECT *
 FROM children
